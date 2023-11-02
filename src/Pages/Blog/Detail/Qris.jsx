719bac1b62cf171cf10b9972a79e4c9acca131b0
Fabyan Yastika Permana <fabyanxrpl@gmail.com>
2023-11-02 17:50:46 +0700

    add blog detail

--- src/Pages/Blog/Detail/Qris.jsx
+++ src/Pages/Blog/Detail/Qris.jsx
@@ -3,7 +3,12 @@ import { Link } from "react-router-dom";
 const Qris = () => {
   return (
     <>
-      <div className="bg-primary w-full h-full px-16 pt-20">
+      <img
+        src="/images/blog/detail/bg.jpg"
+        className="w-full absolute h-[615px] -z-10 top-0"
+        alt=""
+      />
+      <div className="bg-gradient-to-l from-white to-primary/70 w-full h-full py-10 px-16 pt-20">
         <Link
           to="/blog"
           className="flex items-center space-x-2 text-2xl text-white"
@@ -11,7 +16,21 @@ const Qris = () => {
           <BsArrowLeftShort />
           <h1>Back to Blog</h1>
         </Link>
+
+        <div className="mt-20">
+          <div className="flex space-x-20">
+            <h1 className="text-7xl font-bold text-slate-700">
+              Transaksi Lintas Negara? Bukan Jadi Masalah! Yuk Kenalan Sama QRIS
+              Cross-Border!
+            </h1>
+            <img src="/images/blog/detail/mon2.gif" className="w-96" alt="" />
+          </div>
+        </div>
       </div>
+      <div className="-mt-20 px-16">
+        <div className="bg-white hover:border-2 hover:border-primary duration-150 hover:scale-[1.05] w-[600px] rounded-lg shadow-xl h-96"></div>
+      </div>
+      <div className="mt-96"></div>
     </>
   );
 };
