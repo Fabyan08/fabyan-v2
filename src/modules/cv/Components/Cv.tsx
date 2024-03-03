7b17afe32e68416469667073f2a5070f3c567a3a
Fabyan Yastika Permana <fabyanxrpl@gmail.com>
2024-03-03 21:46:29 +0700

    fix import

--- src/modules/cv/Components/Cv.tsx
+++ src/modules/cv/Components/Cv.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { IoCloseSharp } from "react-icons/io5";
 import { MdOutlineFileDownload } from "react-icons/md";
 import { Link } from "react-router-dom";
